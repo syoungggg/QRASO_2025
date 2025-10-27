61eb146c3e39f60de6debd0b1b370f5d34a2a89d
heo <heosy3374@daegu.ac.kr>
2025-10-27 22:58:59 +0900

    fix: use persistent /data volume for SQlite

--- qr-backend/app.py
+++ qr-backend/app.py
@@ -34,7 +34,7 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-DB_PATH = os.path.join("/tmp", "reports.db")
+DB_PATH = os.path.join("/data", "reports.db")
 
 # -------------------
 # DB 초기화 (컬럼 자동 추가)
